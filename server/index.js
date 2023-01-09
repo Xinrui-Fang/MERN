cd6e2b22263db14ba248f27b2e365ab255e08f76
Xinrui Fang <xinrui.fang@keio.jp>
2023-01-09 18:58:47 +0900

    first commit

---
+++ server/index.js
@@ -0,0 +1,70 @@
+// Development = Node.js server + React server
+
+// MEN
+// cd server
+// npm init
+
+// E -Express (Which allows our server has a easier looking syntax)
+// npm i express
+// npm i nodemon --save-dev (nodemon will restart the server and reflect the changes)
+// add dev script in the package.json
+// how to run nodemon: npx nodemon
+
+// npm i cors -> cross origin resources sharing
+// npm i mongoose -> mongoDB
+
+// createa "models" folder -> for mongoDB
+
+// Front-end install react-router-dom
+// Back-end install jsonwebtoken -> sign jwt token, retrive them and work with them
+// token is encrypted base64 encoded text, json payload
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
+
+app.use(cors());
+app.use(express.json()); // middleware?
+
+mongoose.connect("mongodb://localhost:27017/mern-stack");
+
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: "OK" });
+  } catch (err) {
+    res.json({ status: "error", error: "Duplicate email" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      "secret123"
+    );
+    return res.json({ status: "ok", user: token });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
+});
+
+app.listen(1337, () => {
+  console.log("hello");
+});
