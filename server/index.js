b7439328e736c56f942c24d8a8fcf6dab8e78113
Xinrui Fang <xinrui.fang@keio.jp>
2023-01-09 22:06:45 +0900

    JWT

--- server/index.js
+++ server/index.js
@@ -59,12 +59,44 @@ app.post("/api/login", async (req, res) => {
       },
       "secret123"
     );
+    console.log(token);
+    res.cookie("x-access-token", token);
     return res.json({ status: "ok", user: token });
   } else {
     return res.json({ status: "error", user: false });
   }
 });
 
+app.get("/api/quote", async (req, res) => {
+  const token = req.headers["x-access-token"];
+
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    const email = decoded.email;
+    const user = await User.findOne({ email: email });
+
+    return res.json({ status: "ok", quote: user.quote });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "get invalid token" });
+  }
+});
+
+app.post("/api/quote", async (req, res) => {
+  const token = req.headers["x-access-token"];
+
+  try {
+    const decoded = jwt.verify(token, "secret123");
+    const email = decoded.email;
+    await User.updateOne({ email: email }, { $set: { quote: req.body.quote } });
+
+    return res.json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: " post invalid token" });
+  }
+});
+
 app.listen(1337, () => {
-  console.log("hello");
+  console.log("Server started on 1337");
 });
